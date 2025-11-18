e16a7907f9f09a87bfd5c6fb3e7262f871e6b22d
Anna-Maria <40987@ma-web.nl>
2025-11-18 12:44:50 +0100

    inelevern

--- 01_Functions/function/Program.cs
+++ 01_Functions/function/Program.cs
@@ -13,6 +13,52 @@ namespace function
         internal void Run()
         {
             Console.WriteLine("Dit is nu de start van mijn programma.");
+            Vraag1();
+            Vraag2();
+            Vraag3();
+            Vraag4();
+            Vraag5();
+        }
+
+        internal void Vraag1()
+        {
+            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+
+        }
+
+        internal void Vraag2()
+        {
+            Console.WriteLine("What would you do if you won a milion dollars?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag3()
+        {
+            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+            string antwoord1 = Console.ReadLine();
+
+            Console.WriteLine(antwoord1);
+        }
+
+        internal void Vraag4()
+        {
+            Console.WriteLine("Have you ever tried talking to animals?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
+        }
+
+        internal void Vraag5()
+        {
+            Console.WriteLine("Has someone caught you dancing in front of the mirror?");
+            string antwoord = Console.ReadLine();
+
+            Console.WriteLine(antwoord);
         }
     }
 }
