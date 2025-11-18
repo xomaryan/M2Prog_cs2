9a0a5ba3c57eae3744ffe754028bf02d5d9101e2
Anna-Maria <40987@ma-web.nl>
2025-11-18 12:18:35 +0100

    inleveren

---
+++ 01_Functions/function/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace function
+{
+        internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Program program = new Program(); //Variable van type Program is program.    //Program() is function constructor.
+            program.Run(); //HIer roep ik met Run().  //Run is een functie.   //Return type is void.
+        }
+
+        internal void Run()
+        {
+            Console.WriteLine("Dit is nu de start van mijn programma.");
+        }
+    }
+}
