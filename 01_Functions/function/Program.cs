cddb5ec21d7c1fda25f959cd6f197da2ec7426db
Anna-Maria <40987@ma-web.nl>
2026-01-02 14:49:30 +0100

    inleveren

--- 01_Functions/function/Program.cs
+++ 01_Functions/function/Program.cs
@@ -1,110 +1,118 @@
 ﻿using System;
 
-namespace function
+namespace function;
+
+class Program
 {
-        internal class Program
+    string[] vragen = new string[]
     {
+        "What 1997 N64 vaideo game feature James Bond and is named after the 1995 film?",
+        "What arcade game was named Puckman in Japan?",
+        "If you can still remember, what are your funniest childhood memories?",
+        "What would you do if you won a million dollars?"
+    };
+    internal string GetVraag(int vraagIndex)
+    {
+        return vragen[vraagIndex];
+    }
 
-        string[] vragen = new string[]{
-            "What 1997 N64 vaideo game feature James Bond and is named after the 1995 film?",
-            "What arcade game was named Puckman in Japan?"
-        };
+    internal string GetRandomVraag()
+    {
+        Random random = new Random();
+        int randomIndex = random.Next(0, vragen.Length);
+        return GetVraag(randomIndex);
+    }
+    internal void Vraag1()
+    {
+        Console.WriteLine("Hoe long do you think you'd survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
 
-        internal string GetVraag(int vraagIndex)
-        {
-            return vragen[vraagIndex];
-        }
+        Console.WriteLine(antwoord);
+    }
 
-        internal string GetRandomVraag()
-        {
-            Random random = new Random();
-            int random1 = random.Next(vragen.Length);
-            return GetVraag(random1);
-        }
-    
+    internal void Vraag2()
+    {
+        Console.WriteLine("What secret conspiracy would you like to actually start letting other people know?");
+        string antwoord = Console.ReadLine();
 
-        static void Main(string[] args)
-        {
-            Program program = new Program(); //Variable van type Program is program.    //Program() is function constructor.
-            program.Run(); //HIer roep ik met Run().  //Run is een functie.   //Return type is void.
-        }
+        Console.WriteLine(antwoord);
+    }
 
-        internal void Run()
-        {
-            Console.WriteLine("Dit is nu de start van mijn programma.");
-            Vraag1();
-            Vraag2();
-            Vraag3();
-            Vraag4();
-            Vraag5();
-            string antwoord7 = Vraag7();
-            Console.WriteLine(antwoord7);
-            string vraag0 = GetVraag(0);
-            Console.WriteLine(vraag0);
-            string randomVraag = GetRandomVraag(); 
-            Console.WriteLine(randomVraag); 
-            string antwoord05 = AskRandomQuestion();
-            Console.WriteLine(antwoord05);
-            
-        }
+    internal void Vraag3()
+    {
+        Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
+        string antwoord = Console.ReadLine();
 
-        internal void Vraag1() 
-        {
-            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
-            string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
+    }
 
-            Console.WriteLine(antwoord);
+    internal void Vraag4()
+    {
+        Console.WriteLine("What would you do if you won a million dollars?");
+        string antwoord = Console.ReadLine();
 
-        }
+        Console.WriteLine(antwoord);
+    }
 
-        internal void Vraag2()
-        {
-            Console.WriteLine("What would you do if you won a milion dollars?");
-            string antwoord = Console.ReadLine();
+    internal void Vraag5()
+    {
+        Console.WriteLine("What is the worst game you played but that you liked anyway?");
+        string antwoord = Console.ReadLine();
 
-            Console.WriteLine(antwoord);
-        }
+        Console.WriteLine(antwoord);
+    }
 
-        internal void Vraag3()
-        {
-            Console.WriteLine("If you can still remember, what are your funniest childhood memories?");
-            string antwoord1 = Console.ReadLine();
+    internal void Vraag6()
+    {
+        Console.WriteLine("Why do round pizzas come in square boxes?");
+        string antwoord = Console.ReadLine();
 
-            Console.WriteLine(antwoord1);
-        }
+        Console.WriteLine(antwoord);
+    }
 
-        internal void Vraag4()
-        {
-            Console.WriteLine("Have you ever tried talking to animals?");
-            string antwoord = Console.ReadLine();
+    internal string Vraag7()
+    {
+        Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
+        string antwoord = Console.ReadLine();
 
-            Console.WriteLine(antwoord);
-        }
+        return antwoord;
+    }
+    static void Main(string[] args)
+    {
+        Program program = new Program(); 
+        program.Run(); 
+    }
 
-        internal void Vraag5()
-        {
-            Console.WriteLine("Has someone caught you dancing in front of the mirror?");
-            string antwoord = Console.ReadLine();
+    internal void Run()
+    {
+        Console.WriteLine("Dit is nu de start van mijn programma");
+        Vraag1();
+        Vraag2();
+        Vraag3();
+        Vraag4();
+        Vraag5();
+        Vraag6();
 
-            Console.WriteLine(antwoord);
-        }
+        string antwoord7 = Vraag7();
+        Console.WriteLine(antwoord7);
 
-        internal string Vraag7()
-        {
-            Console.WriteLine("How long do you think you'd survive in a zombie apocalypse?");
-            string antwoord = Console.ReadLine();
+        string vraag0 = GetVraag(0);
+        Console.WriteLine(vraag0);
 
-            return antwoord;
+        string randomVraag = GetRandomVraag();
+        Console.WriteLine(randomVraag);
 
-        }
-        
-        internal string AskRandomQuestion()
-        {
-            Console.WriteLine("What is your favorite color?");
-            string GetRandomVraag = Console.ReadLine();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+        AskRandomQuestion();
+    }
 
-            return GetRandomVraag;
-        } 
-        
+    internal void AskRandomQuestion()
+    {
+        string deVraag = GetRandomVraag();
+        Console.WriteLine(deVraag);
+        string antwoord = Console.ReadLine();
+        Console.WriteLine(antwoord);
     }
 }
