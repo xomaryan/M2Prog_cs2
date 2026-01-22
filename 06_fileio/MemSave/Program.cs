118dd9b70af5387080880cf2572725543b3d2a8c
Anna-Maria <40987@ma-web.nl>
2026-01-22 20:39:05 +0100

    inleveren

---
+++ 06_fileio/MemSave/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace MemSave;
+
+class Program
+{
+    string welkomstTekst = "hello World";
+    string saveFile = "welkomState.txt";
+    static void Main(string[] args)
+    {  
+        Program program = new Program();
+        program.Run();  
+    }
+
+    internal void Run()
+    {
+        if (File.Exists(saveFile))
+        {
+            welkomstTekst = File.ReadAllText(saveFile);
+        }
+
+        while (true)
+        {
+            Console.WriteLine(welkomstTekst);
+            string invoer = Console.ReadLine();
+
+            if (invoer.Length > 0)
+            {
+                welkomstTekst = invoer;
+                File.WriteAllText(saveFile, welkomstTekst);
+            }
+        }
+    }
+
+    
+}
