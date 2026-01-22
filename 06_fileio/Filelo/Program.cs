eb545747eb8c4cc457731a56213fff3878f7d838
Anna-Maria <40987@ma-web.nl>
2026-01-22 15:08:00 +0100

    inleveren

--- 06_fileio/Filelo/Program.cs
+++ 06_fileio/Filelo/Program.cs
@@ -4,7 +4,19 @@ class Program
 {
     internal void Run()
     {
-        
+        Console.WriteLine(Environment.CurrentDirectory);
+        string[] lines = File.ReadAllLines("quiz.txt");
+        foreach (string line in lines)
+        {
+            Console.WriteLine(line);
+        }
+
+        Console.WriteLine(Environment.CurrentDirectory);
+        string[] lines2 = File.ReadAllLines("quiz1.txt");
+        foreach (string line in lines2)
+        {
+            Console.WriteLine(line);
+        }
     }
     static void Main(string[] args)
     {
